793000fdb9a4a54ee9f4ad9afc545ffc490de50f
jsaluja09 <147002654+jsaluja09@users.noreply.github.com>
2024-08-07 19:44:31 -0400

    exercise3

---
+++ exercise3.java
@@ -0,0 +1,9 @@
+public class exercise3 {
+
+    public static void main(String[] args) {
+        int x = 124;
+        x = (x < 100) ? (x + 50) : (x / 20);
+        System.out.println(x);
+    }
+    
+}
