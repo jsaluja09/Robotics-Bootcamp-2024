b0cfe0bbe8f2350943f29b04c68f6d90cd1c0eb5
jsaluja09 <147002654+jsaluja09@users.noreply.github.com>
2024-08-07 19:21:01 -0400

    exercise 2

---
+++ exercise2.java
@@ -0,0 +1,12 @@
+public class exercise2 {
+    public static void main(String[] args) {
+        int x = 672;
+        int y = 46732;
+
+        x++;
+        y--;
+
+        int z = x + y;
+        System.out.println(z);
+    }
+}
