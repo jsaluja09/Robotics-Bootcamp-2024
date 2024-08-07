cbf2fba24890700556e997a38abf5b68ee5caa85
jsaluja09 <147002654+jsaluja09@users.noreply.github.com>
2024-08-07 18:59:59 -0400

    code

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int mutableInt = 433;
+        String IMMUTABLE_STRING = "Hello";
+        System.out.println(IMMUTABLE_STRING);
+        System.out.println(mutableInt);
+    }
+}
