bb058546cd8fa950ce3e5684ecb67558d8ad398b
jsaluja09 <147002654+jsaluja09@users.noreply.github.com>
2024-08-08 19:23:11 -0400

    Updated

---
+++ action.java
@@ -0,0 +1,11 @@
+public class action {
+
+    
+    public static void main(String[] args) {
+        person.print_age();
+        person.make_older();
+        person.print_age();
+        person.make_younger();
+        person.print_age();
+    }
+}
---
+++ person.java
@@ -0,0 +1,15 @@
+public class person {
+    public static int age = 16;
+
+    public static void print_age() {
+        System.out.println("Age: " + age);
+    }
+    
+    public static  void make_older() {
+        age++;
+    }
+
+    public static void make_younger() {
+        age--;
+    }
+}
